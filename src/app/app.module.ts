0eaca8f2f5b328dbab83d5fa70e93807b6809d0a
Rafael Rodrigues <rafaelsilvarodrigues_@hotmail.com>
2022-08-18 20:21:23 -0400

    Projeto Angular criado na Aula2 Mentoria-DevForTech

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
